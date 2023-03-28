243f8f46e994b05d6f43871d2da31e744047c08e
Lior Vinman <liorvi35@gmail.com>
2023-03-28 21:27:48 +0300

    Update player.cpp

--- sources/player.cpp
+++ sources/player.cpp
@@ -2,7 +2,7 @@
  * this file contains the implementation for player object functions
  * Assignment 2, Software Systems 2 course at Ariel University
  * @author Lior Vinman
- * @date 27.03.2023
+ * @date 28.03.2023
 */
 
 #include <iostream>
@@ -13,6 +13,15 @@ using namespace std;
 
 namespace ariel
 {
+    // defult-ctor
+    Player::Player()
+    {
+        this->_pName = "User";
+        this->_isWon = false;
+        this->_cardsCounter = 0;
+        this->_stackLenght = 0;
+    }
+
     // standard-ctor
     Player::Player(string name)
     {
@@ -22,6 +31,45 @@ namespace ariel
         this->_stackLenght = 0;
     }
 
-    // defult destructor
-    Player::~Player() {}
+    // copying-ctor
+    Player::Player(const Player &other)
+    {
+        this->_pName = other._pName;
+        this->_isWon = other._isWon;
+        this->_cardsCounter = other._cardsCounter;
+        this->_stackLenght = other._stackLenght;
+    }
+
+    // move-ctor
+    Player::Player(Player &&other) noexcept:
+        _pName(std::move(other._pName)),
+        _isWon(std::move(other._isWon)),
+        _cardsCounter(std::move(other._cardsCounter)),
+        _stackLenght(std::move(other._stackLenght)) {}
+
+    // copying operator
+    Player& Player::operator=(const Player& other)
+    {
+        if (this != &other) 
+        {
+            this->_pName = other._pName;
+            this->_isWon = other._isWon;
+            this->_cardsCounter = other._cardsCounter;
+            this->_stackLenght = other._stackLenght;
+        }
+        return *this;
+    }
+
+    // moving operator
+    Player& Player::operator=(Player&& other) noexcept
+    {
+        if (this != &other)
+        {
+            this->_pName = std::move(other._pName);
+            this->_isWon = std::move(other._isWon);
+            this->_cardsCounter = std::move(other._cardsCounter);
+            this->_stackLenght = std::move(other._stackLenght);
+        }
+        return *this;
+    }
 };
