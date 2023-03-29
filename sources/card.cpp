28856478bb864fcbd58f0c9562ccd3dad2f1ccd9
Lior Vinman <liorvi35@gmail.com>
2023-03-29 18:23:10 +0300

    Update card.cpp

--- sources/card.cpp
+++ sources/card.cpp
@@ -2,7 +2,7 @@
  * this file contains the implementation for card object functions
  * Assignment 2, Software Systems 2 course at Ariel University
  * @author Lior Vinman
- * @date 28.03.2023
+ * @date 29.03.2023
 */
 
 #include <iostream>
@@ -12,52 +12,23 @@
 using namespace std;
 
 namespace ariel
-{   
-    // defult-ctor
-    Card::Card()
-    {
-        this->_value = 1;
-        this->_type = DIAMOND;
-    }
-
-    // standard-ctor
+{
+    // ctor
     Card::Card(int value, CardType type)
     {
         this->_value = value;
         this->_type = type;
     }
 
-    // copy-ctor
-    Card::Card(const Card &other)
+    // getter for card's value
+    int Card::getValue()
     {
-        this->_type = other._type;
-        this->_value = other._value;
+        return this->_value;
     }
 
-    // move-ctor
-    Card::Card(Card &&other) noexcept:
-        _type(std::move(other._type)),
-        _value(std::move(other._value)) {}
-
-    // copying operator
-    Card& Card::operator=(const Card& other)
+    // getter for card's symbol
+    char Card::getType()
     {
-        if (this != &other) 
-        {
-            this->_value = other._value;
-            this->_type = other._type;
-        }
-        return *this;
+        return this->_type;
     }
-
-    // moving operator
-    Card& Card::operator=(Card&& other) noexcept
-    {
-        if (this != &other)
-        {
-            this->_value = std::move(other._value);
-            this->_type = std::move(other._type);
-        }
-        return *this;
-    }
-};
+}
