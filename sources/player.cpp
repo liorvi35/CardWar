b587b8fa7cf7f94121ae5122c89dfe18ac29b3aa
Lior Vinman <liorvi35@gmail.com>
2023-03-29 18:24:30 +0300

    Update player.cpp

--- sources/player.cpp
+++ sources/player.cpp
@@ -2,7 +2,7 @@
  * this file contains the implementation for player object functions
  * Assignment 2, Software Systems 2 course at Ariel University
  * @author Lior Vinman
- * @date 28.03.2023
+ * @date 29.03.2023
 */
 
 #include <iostream>
@@ -14,62 +14,41 @@ using namespace std;
 namespace ariel
 {
     // defult-ctor
-    Player::Player()
-    {
-        this->_pName = "User";
-        this->_isWon = false;
-        this->_cardsCounter = 0;
-        this->_stackLenght = 0;
-    }
-
-    // standard-ctor
     Player::Player(string name)
     {
         this->_pName = name;
-        this->_isWon = false;
+        this->_playingGame = false;
         this->_cardsCounter = 0;
-        this->_stackLenght = 0;
+        this->_stackLenght = 26;
     }
 
-    // copying-ctor
-    Player::Player(const Player &other)
+    // getter for player name 
+    string Player::getPlayerName()
     {
-        this->_pName = other._pName;
-        this->_isWon = other._isWon;
-        this->_cardsCounter = other._cardsCounter;
-        this->_stackLenght = other._stackLenght;
+        return this->_pName;
     }
 
-    // move-ctor
-    Player::Player(Player &&other) noexcept:
-        _pName(std::move(other._pName)),
-        _isWon(std::move(other._isWon)),
-        _cardsCounter(std::move(other._cardsCounter)),
-        _stackLenght(std::move(other._stackLenght)) {}
-
-    // copying operator
-    Player& Player::operator=(const Player& other)
+    // getter for cards counter
+    int Player::cardesTaken()
     {
-        if (this != &other) 
-        {
-            this->_pName = other._pName;
-            this->_isWon = other._isWon;
-            this->_cardsCounter = other._cardsCounter;
-            this->_stackLenght = other._stackLenght;
-        }
-        return *this;
+        return this->_cardsCounter;
     }
 
-    // moving operator
-    Player& Player::operator=(Player&& other) noexcept
+    // getter for field that indicating if player inside game
+    bool Player::getIfPlaying()
     {
-        if (this != &other)
-        {
-            this->_pName = std::move(other._pName);
-            this->_isWon = std::move(other._isWon);
-            this->_cardsCounter = std::move(other._cardsCounter);
-            this->_stackLenght = std::move(other._stackLenght);
-        }
-        return *this;
+        return this->_playingGame;
     }
-};
+
+    // getter for stack size
+    int Player::stacksize()
+    {
+        return this->_stackLenght;
+    }
+
+    // setter for changing game mode
+    void Player::setIfPlaying(bool change)
+    {
+        this->_playingGame = change;
+    }
+}
